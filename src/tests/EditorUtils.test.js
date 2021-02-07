7dc45509e4ad87e41d63ff1bdbc2021c539a3ead
res876ttb <someone10851222@gmail.com>
2021-02-07 21:32:28 +0800

    Generalize styler function

--- src/tests/EditorUtils.test.js
+++ src/tests/EditorUtils.test.js
@@ -12,8 +12,8 @@
  *************************************************/
 import {
   stylerConst,
-  findBold,
-  stylerBold,
+  findStylerPair,
+  styleToggler,
 } from '../utils/EditorUtils.js';
 
 /*************************************************
@@ -31,23 +31,23 @@ test(`Find bold function`, () => {
   const PRE = stylerConst.PREFIX_BOLD;
 
   // 1. Middle
-  testResult = findBold(`abcd${PRE}efg${POST}higj`);
+  testResult = findStylerPair(`abcd${PRE}efg${POST}higj`, PRE, POST);
   expect(testResult).toStrictEqual([{start: 8, end: 10}]);
 
   // 2. 2 continuous bold
-  testResult = findBold(`abcd${PRE}efg${POST}${PRE}higj${POST}klm`);
+  testResult = findStylerPair(`abcd${PRE}efg${POST}${PRE}higj${POST}klm`, PRE, POST);
   expect(testResult).toStrictEqual([{start: 8, end: 10}, {start: 19, end: 22}]);
 
   // 3. only bold
-  testResult = findBold(`${PRE}efg${POST}`);
+  testResult = findStylerPair(`${PRE}efg${POST}`, PRE, POST);
   expect(testResult).toStrictEqual([{start: 4, end: 6}]);
 
   // 4. Most left & most right
-  testResult = findBold(`${PRE}efg${POST}123${PRE}efg${POST}`);
+  testResult = findStylerPair(`${PRE}efg${POST}123${PRE}efg${POST}`, PRE, POST);
   expect(testResult).toStrictEqual([{start: 4, end: 6}, {start: 18, end: 20}]);
 
   // 5. Nothing
-  testResult = findBold(`abc`);
+  testResult = findStylerPair(`abc`, PRE, POST);
   expect(testResult).toStrictEqual([]);
 });
 
@@ -62,85 +62,135 @@ test(`Toggle bold`, () => {
   // 1 Remove bold
   // 1.1 Exactly overlapped
   // 1.1.1 Middle
-  testResult = stylerBold(`0123${PRE}456${POST}789`, 4 + PRE_LEN, 6 + PRE_LEN);
+  testResult = styleToggler(`0123${PRE}456${POST}789`, 4 + PRE_LEN, 6 + PRE_LEN, PRE, POST);
   expect(testResult).toStrictEqual(`0123456789`);
 
   // 1.1.2 Left
-  testResult = stylerBold(`${PRE}012${POST}3456`, PRE_LEN, 2 + PRE_LEN);
+  testResult = styleToggler(`${PRE}012${POST}3456`, PRE_LEN, 2 + PRE_LEN, PRE, POST);
   expect(testResult).toStrictEqual(`0123456`);
 
   // 1.1.3 Right
-  testResult = stylerBold(`0123${PRE}456${POST}`, 4 + PRE_LEN, 6 + PRE_LEN);
+  testResult = styleToggler(`0123${PRE}456${POST}`, 4 + PRE_LEN, 6 + PRE_LEN, PRE, POST);
   expect(testResult).toStrictEqual(`0123456`);
 
   // 1.1.4 1 char
-  testResult = stylerBold(`0123${PRE}4${POST}5678`, 4 + PRE_LEN, 4 + PRE_LEN);
+  testResult = styleToggler(`0123${PRE}4${POST}5678`, 4 + PRE_LEN, 4 + PRE_LEN, PRE, POST);
   expect(testResult).toStrictEqual(`012345678`);
 
   // 1.2 Overlapped by another bigger range
   // 1.2.1 Middle
-  testResult = stylerBold(`0123${PRE}45678${POST}9012`, 5 + PRE_LEN, 7 + PRE_LEN);
+  testResult = styleToggler(`0123${PRE}45678${POST}9012`, 5 + PRE_LEN, 7 + PRE_LEN, PRE, POST);
   expect(testResult).toStrictEqual(`0123${PRE}4${POST}567${PRE}8${POST}9012`);
 
   // 1.2.2 Left
-  testResult = stylerBold(`0123${PRE}45678${POST}9012`, 4 + PRE_LEN, 7 + PRE_LEN);
+  testResult = styleToggler(`0123${PRE}45678${POST}9012`, 4 + PRE_LEN, 7 + PRE_LEN, PRE, POST);
   expect(testResult).toStrictEqual(`01234567${PRE}8${POST}9012`);
 
   // 1.2.3. Right
-  testResult = stylerBold(`0123${PRE}45678${POST}9012`, 5 + PRE_LEN, 8 + PRE_LEN);
+  testResult = styleToggler(`0123${PRE}45678${POST}9012`, 5 + PRE_LEN, 8 + PRE_LEN, PRE, POST);
   expect(testResult).toStrictEqual(`0123${PRE}4${POST}56789012`);
 
   // 2 Add bold
   // 2.1 No overlap
   // 2.1.1 Middle
-  testResult = stylerBold(`012345678`, 2, 4);
+  testResult = styleToggler(`012345678`, 2, 4, PRE, POST);
   expect(testResult).toStrictEqual(`01${PRE}234${POST}5678`);
-  testResult = stylerBold(`${PRE}0${POST}123456${PRE}78${POST}`, 2 + PRE_LEN + POST_LEN, 4 + PRE_LEN + POST_LEN);
+  testResult = styleToggler(`${PRE}0${POST}123456${PRE}78${POST}`, 2 + PRE_LEN + POST_LEN, 4 + PRE_LEN + POST_LEN, PRE, POST);
   expect(testResult).toStrictEqual(`${PRE}0${POST}1${PRE}234${POST}56${PRE}78${POST}`);
 
   // 2.1.2 Left
-  testResult = stylerBold(`012345678`, 0, 4);
+  testResult = styleToggler(`012345678`, 0, 4, PRE, POST);
   expect(testResult).toStrictEqual(`${PRE}01234${POST}5678`);
-  testResult = stylerBold(`0123456${PRE}78${POST}`, 0, 4);
+  testResult = styleToggler(`0123456${PRE}78${POST}`, 0, 4, PRE, POST);
   expect(testResult).toStrictEqual(`${PRE}01234${POST}56${PRE}78${POST}`);
 
   // 2.1.3 Right
-  testResult = stylerBold(`012345678`, 2, 8);
+  testResult = styleToggler(`012345678`, 2, 8, PRE, POST);
   expect(testResult).toStrictEqual(`01${PRE}2345678${POST}`);
-  testResult = stylerBold(`${PRE}0${POST}12345678`, 2 + PRE_LEN + POST_LEN, 8 + PRE_LEN + POST_LEN);
+  testResult = styleToggler(`${PRE}0${POST}12345678`, 2 + PRE_LEN + POST_LEN, 8 + PRE_LEN + POST_LEN, PRE, POST);
   expect(testResult).toStrictEqual(`${PRE}0${POST}1${PRE}2345678${POST}`);
 
   // 2.2 Left partial overlapping
-  testResult = stylerBold(`${PRE}0123${POST}45678`, 2 + PRE_LEN, 4 + PRE_LEN + POST_LEN);
+  testResult = styleToggler(`${PRE}0123${POST}45678`, 2 + PRE_LEN, 4 + PRE_LEN + POST_LEN, PRE, POST);
   expect(testResult).toStrictEqual(`${PRE}01234${POST}5678`);
-  testResult = stylerBold(`${PRE}0123${POST}456${PRE}78${POST}`, 2 + PRE_LEN, 4 + PRE_LEN + POST_LEN);
+  testResult = styleToggler(`${PRE}0123${POST}456${PRE}78${POST}`, 2 + PRE_LEN, 4 + PRE_LEN + POST_LEN, PRE, POST);
   expect(testResult).toStrictEqual(`${PRE}01234${POST}56${PRE}78${POST}`);
 
   // 2.3 Right partial overlapping
-  testResult = stylerBold(`0123${PRE}456${POST}78`, 2, 4 + PRE_LEN);
+  testResult = styleToggler(`0123${PRE}456${POST}78`, 2, 4 + PRE_LEN, PRE, POST);
   expect(testResult).toStrictEqual(`01${PRE}23456${POST}78`);
 
   // 2.4 A small bold in the middle
-  testResult = stylerBold(`012${PRE}3${POST}45678`, 2, 4 + PRE_LEN + POST_LEN);
+  testResult = styleToggler(`012${PRE}3${POST}45678`, 2, 4 + PRE_LEN + POST_LEN, PRE, POST);
   expect(testResult).toStrictEqual(`01${PRE}234${POST}5678`);
 
   // 2.5 Left & right partial overlapping
-  testResult = stylerBold(`01${PRE}23${POST}45${PRE}67${POST}8`, 3 + PRE_LEN, 6 + PRE_LEN * 2 + POST_LEN);
+  testResult = styleToggler(`01${PRE}23${POST}45${PRE}67${POST}8`, 3 + PRE_LEN, 6 + PRE_LEN * 2 + POST_LEN, PRE, POST);
   expect(testResult).toStrictEqual(`01${PRE}234567${POST}8`);
 
   // 2.6 Left partial overlapping with a small bold in the middle
-  testResult = stylerBold(`0${PRE}12${POST}3${PRE}4${POST}5678`, 2 + PRE_LEN, 5 + PRE_LEN * 2 + POST_LEN * 2);
+  testResult = styleToggler(`0${PRE}12${POST}3${PRE}4${POST}5678`, 2 + PRE_LEN, 5 + PRE_LEN * 2 + POST_LEN * 2, PRE, POST);
   expect(testResult).toStrictEqual(`0${PRE}12345${POST}678`);
 
   // 2.7 Right partial overlapping with a small bold in the middle
-  testResult = stylerBold(`0123${PRE}4${POST}5${PRE}67${POST}8`, 3, 6 + PRE_LEN * 2 + POST_LEN);
+  testResult = styleToggler(`0123${PRE}4${POST}5${PRE}67${POST}8`, 3, 6 + PRE_LEN * 2 + POST_LEN, PRE, POST);
   expect(testResult).toStrictEqual(`012${PRE}34567${POST}8`);
 
   // 2.8 Left & right partial overlapping with a small bold in the middle
-  testResult = stylerBold(`0${PRE}12${POST}3${PRE}4${POST}5${PRE}67${POST}8`, 2 + PRE_LEN, 6 + PRE_LEN * 3 + POST_LEN * 2);
+  testResult = styleToggler(`0${PRE}12${POST}3${PRE}4${POST}5${PRE}67${POST}8`, 2 + PRE_LEN, 6 + PRE_LEN * 3 + POST_LEN * 2, PRE, POST);
   expect(testResult).toStrictEqual(`0${PRE}1234567${POST}8`);
 
   // 2.8 Left & right partial overlapping with 2 small bolds in the middle
-  testResult = stylerBold(`0${PRE}12${POST}3${PRE}4${POST}5${PRE}6${POST}7${PRE}89${POST}012345`, 2 + PRE_LEN, 8 + PRE_LEN * 4 + POST_LEN * 3);
+  testResult = styleToggler(`0${PRE}12${POST}3${PRE}4${POST}5${PRE}6${POST}7${PRE}89${POST}012345`, 2 + PRE_LEN, 8 + PRE_LEN * 4 + POST_LEN * 3, PRE, POST);
   expect(testResult).toStrictEqual(`0${PRE}123456789${POST}012345`);
-})
+});
+
+// Toggle general test cases
+test(`Toggle bold, italic, underline, strikethrough, link, math, code`, () => {
+  let testResult;
+  const PRE_LEN = stylerConst.PREFIX_LEN;
+  const POST_LEN = stylerConst.POSTFIX_LEN;
+  let POST, PRE;
+
+  // 1. BOLD
+  POST = stylerConst.POSTFIX_BOLD;
+  PRE = stylerConst.PREFIX_BOLD;
+  testResult = styleToggler(`0123${PRE}456${POST}789`, 4 + PRE_LEN, 6 + PRE_LEN, PRE, POST);
+  expect(testResult).toStrictEqual(`0123456789`);
+
+  // 2. ITALIC
+  POST = stylerConst.POSTFIX_ITALIC;
+  PRE = stylerConst.PREFIX_ITALIC;
+  testResult = styleToggler(`0123${PRE}456${POST}789`, 4 + PRE_LEN, 6 + PRE_LEN, PRE, POST);
+  expect(testResult).toStrictEqual(`0123456789`);
+
+  // 3. UNDERLINE
+  POST = stylerConst.POSTFIX_UNDERLINE;
+  PRE = stylerConst.PREFIX_UNDERLINE;
+  testResult = styleToggler(`0123${PRE}456${POST}789`, 4 + PRE_LEN, 6 + PRE_LEN, PRE, POST);
+  expect(testResult).toStrictEqual(`0123456789`);
+
+  // 4. STRIKETHROUGH
+  POST = stylerConst.POSTFIX_STRIKETHROUGH;
+  PRE = stylerConst.PREFIX_STRIKETHROUGH;
+  testResult = styleToggler(`0123${PRE}456${POST}789`, 4 + PRE_LEN, 6 + PRE_LEN, PRE, POST);
+  expect(testResult).toStrictEqual(`0123456789`);
+
+  // 5. LINK
+  POST = stylerConst.POSTFIX_LINK;
+  PRE = stylerConst.PREFIX_LINK;
+  testResult = styleToggler(`0123${PRE}456${POST}789`, 4 + PRE_LEN, 6 + PRE_LEN, PRE, POST);
+  expect(testResult).toStrictEqual(`0123456789`);
+
+  // 6. MATH
+  POST = stylerConst.POSTFIX_MATH;
+  PRE = stylerConst.PREFIX_MATH;
+  testResult = styleToggler(`0123${PRE}456${POST}789`, 4 + PRE_LEN, 6 + PRE_LEN, PRE, POST);
+  expect(testResult).toStrictEqual(`0123456789`);
+
+  // 7. CODE
+  POST = stylerConst.POSTFIX_CODE;
+  PRE = stylerConst.PREFIX_CODE;
+  testResult = styleToggler(`0123${PRE}456${POST}789`, 4 + PRE_LEN, 6 + PRE_LEN, PRE, POST);
+  expect(testResult).toStrictEqual(`0123456789`);
+});
--- src/utils/EditorUtils.js
+++ src/utils/EditorUtils.js
@@ -22,6 +22,7 @@ export const stylerEnum = {
   COLOR: 8,
   BACKGROUND: 9,
   MENTION: 10,
+  HYPERLINK: 11,
 }
 
 // Styler style
@@ -35,68 +36,107 @@ export const stylerConst = {
 
   PREFIX_BOLD: "«sb:",
   POSTFIX_BOLD: ":sb»",
+
+  PREFIX_ITALIC: "«si:",
+  POSTFIX_ITALIC: ":si»",
+
+  PREFIX_UNDERLINE: "«su:",
+  POSTFIX_UNDERLINE: ":su»",
+
+  PREFIX_STRIKETHROUGH: "«st:",
+  POSTFIX_STRIKETHROUGH: ":st»",
+
+  PREFIX_LINK: "«sl:",
+  POSTFIX_LINK: ":sl»",
+
+  PREFIX_MATH: "«sm:",
+  POSTFIX_MATH: ":sm»",
+
+  PREFIX_CODE: "«sc:",
+  POSTFIX_CODE: ":sc»",
 };
 
 /*************************************************
  * FUNCTIONS
  *************************************************/
 export function contentStyler(content, type, start, end, param) {
+  let PREFIX, POSTFIX;
+
   switch(type) {
     case stylerEnum.BOLD:
-      return stylerBold(content, start, end);
+      PREFIX = stylerConst.PREFIX_BOLD;
+      POSTFIX = stylerConst.POSTFIX_BOLD;
+      break;
     
     case stylerEnum.ITALIC:
+      PREFIX = stylerConst.PREFIX_ITALIC;
+      POSTFIX = stylerConst.POSTFIX_ITALIC;
       break;
     
     case stylerEnum.UNDERLINE:
+      PREFIX = stylerConst.PREFIX_UNDERLINE;
+      POSTFIX = stylerConst.POSTFIX_UNDERLINE;
       break;
     
     case stylerEnum.STRIKETHROUGH:
+      PREFIX = stylerConst.PREFIX_STRIKETHROUGH;
+      POSTFIX = stylerConst.POSTFIX_STRIKETHROUGH;
       break;
     
     case stylerEnum.LINK:
+      PREFIX = stylerConst.PREFIX_LINK;
+      POSTFIX = stylerConst.POSTFIX_LINK;
       break;
       
     case stylerEnum.CODE:
+      PREFIX = stylerConst.PREFIX_CODE;
+      POSTFIX = stylerConst.POSTFIX_CODE;
       break;
     
     case stylerEnum.MATH:
+      PREFIX = stylerConst.PREFIX_MATH;
+      POSTFIX = stylerConst.POSTFIX_MATH;
       break;
     
-    case stylerEnum.COLOR:
+    case stylerEnum.COLOR:      // TODO
       break;
 
-    case stylerEnum.BACKGROUND:
+    case stylerEnum.BACKGROUND: // TODO
+      break;
+
+    case stylerEnum.HYPERLINK:  // TODO
       break;
     
-    case stylerEnum.MENTION: // ???
+    case stylerEnum.MENTION:    // TODO: pages, blocks, users, dates
       break;
     
     default:
       console.error('Unknown type:', type);
       break;
   }
+
+  return styleToggler(content, start, end, PREFIX, POSTFIX);
 }
 
 /**
- * @function stylerBold
+ * @function styleToggler
  * @description Toggle string bold status in content.
  * @param {string} content 
  * @param {int} start 
  * @param {int} end 
  * «sb:bold»
  */
-export function stylerBold(content, start, end) {
-  let boldList = findBold(content); // [{start, end}, {start, end}] <- position of start/end char
+export function styleToggler(content, start, end, PREFIX, POSTFIX) {
+  let pairList = findStylerPair(content, PREFIX, POSTFIX); // return: [{start, end}, {start, end}] <- position of start/end char
 
   /**
-   * Remove bold
+   * Remove style
    * 1. Exactly overlapped
    * 2. Overlapped by another bigger range
    */
-  for (let i = 0; i < boldList.length; i++) {
+  for (let i = 0; i < pairList.length; i++) {
     // 1. Exactly overlapped
-    if (start == boldList[i].start && end == boldList[i].end) {
+    if (start == pairList[i].start && end == pairList[i].end) {
       let part1 = content.substring(0, start - stylerConst.PREFIX_LEN);
       let part2 = content.substring(start, end + 1);
       let part3 = content.substring(end + 1 + stylerConst.POSTFIX_LEN);
@@ -104,23 +144,23 @@ export function stylerBold(content, start, end) {
     }
 
     // 2. Overlapped by another bigger range
-    if (start >= boldList[i].start && end <= boldList[i].end) {
+    if (start >= pairList[i].start && end <= pairList[i].end) {
       let res = '', tmp = '';
       // Remove start
-      if (boldList[i].start == start) {
+      if (pairList[i].start == start) {
         res = content.substring(0, start - stylerConst.PREFIX_LEN);
       } else {
         res = content.substring(0, start);
-        res += stylerConst.POSTFIX_BOLD;
+        res += POSTFIX;
       }
 
       res += content.substring(start, end + 1);
       
       // Remove end
-      if (boldList[i].end == end) {
+      if (pairList[i].end == end) {
         res += content.substring(end + 1 + stylerConst.POSTFIX_LEN);
       } else {
-        res += stylerConst.PREFIX_BOLD;
+        res += PREFIX;
         res += content.substring(end + 1);
       }
 
@@ -129,75 +169,75 @@ export function stylerBold(content, start, end) {
   }
 
   /**
-   * Add bold
+   * Add style
    * 1. Find first bold range that overlaps with current range.
    * 2. Add bold string.
    * 3. Repeat 1 & 2.
    * 4. Remove continuous bold prefix/postfix.
    */
   // console.log(content, start, end);
-  let i = boldList.length - 1;
+  let i = pairList.length - 1;
   let lastEnd = end;
   let res = '';
   for (; i >= 0 && start < lastEnd; i--) {
-    if (boldList[i].end < start) break;
+    if (pairList[i].end < start) break;
 
     // Move lastEnd
-    if (boldList[i].start > start && boldList[i].start <= end && boldList[i].end > end) {
-      lastEnd = boldList[i].start - stylerConst.PREFIX_LEN - 1;
+    if (pairList[i].start > start && pairList[i].start <= end && pairList[i].end > end) {
+      lastEnd = pairList[i].start - stylerConst.PREFIX_LEN - 1;
       res = content.substring(lastEnd + 1);
     }
     // Check whether this bold range overlaps with current range.
-    else if (boldList[i].end < end && boldList[i].end >= start) {
+    else if (pairList[i].end < end && pairList[i].end >= start) {
       if (lastEnd == end) {
         res = content.substring(lastEnd + 1);
       }
       // Add bold string
-      res = stylerConst.PREFIX_BOLD + content.substring(boldList[i].end + stylerConst.POSTFIX_LEN + 1, lastEnd + 1) + stylerConst.POSTFIX_BOLD + res;
-      res = content.substring(boldList[i].start - stylerConst.PREFIX_LEN, boldList[i].end + stylerConst.POSTFIX_LEN + 1) + res;
-      lastEnd = boldList[i].start - stylerConst.PREFIX_LEN - 1;
+      res = PREFIX + content.substring(pairList[i].end + stylerConst.POSTFIX_LEN + 1, lastEnd + 1) + POSTFIX + res;
+      res = content.substring(pairList[i].start - stylerConst.PREFIX_LEN, pairList[i].end + stylerConst.POSTFIX_LEN + 1) + res;
+      lastEnd = pairList[i].start - stylerConst.PREFIX_LEN - 1;
     }
   }
 
   if (lastEnd == end) {
     // No overlapping
-    res = content.substring(0, start) + stylerConst.PREFIX_BOLD + content.substring(start, end + 1) + stylerConst.POSTFIX_BOLD + content.substring(end + 1);
+    res = content.substring(0, start) + PREFIX + content.substring(start, end + 1) + POSTFIX + content.substring(end + 1);
   } else if (lastEnd >= start) {
     // If 1 or more overlapping, but not left partial overlapping
-    res = stylerConst.PREFIX_BOLD + content.substring(start, lastEnd + 1) + stylerConst.POSTFIX_BOLD + res;
+    res = PREFIX + content.substring(start, lastEnd + 1) + POSTFIX + res;
     res = content.substring(0, start) + res;
   } else {
     // Left partial overlapping
-    res = content.substring(0, boldList[i + 1].start - stylerConst.PREFIX_LEN) + res;
+    res = content.substring(0, pairList[i + 1].start - stylerConst.PREFIX_LEN) + res;
   }
 
   // Remove continuous bold symbols
-  res = res.replace(RegExp(`${stylerConst.POSTFIX_BOLD}${stylerConst.PREFIX_BOLD}`, 'g'), '');
+  res = res.replace(RegExp(`${POSTFIX}${PREFIX}`, 'g'), '');
 
   return res;
 }
 
 /**
- * @function findBold
+ * @function findStylerPair
  * @description Find position of bold symbol.
  * @param {string} content 
- * @returns A list of bold string position, which includes the start and end position.
- *          EX: "abcd«sb:efg:sb»higj" will output [[8,10]]
+ * @returns A list of styler string position, which includes the start and end position.
+ *          EX: "abcd«sb:efg:sb»higj" will output [[8,10]] for bold style.
  *                       ^ ^
  */
-export function findBold(content) {
+export function findStylerPair(content, PREFIX, POSTFIX) {
   let result = [];
   let start = -1;
 
   for (let i = 0; i < content.length; i++) {
     if (content[i] == stylerConst.PREFIX) {
-    if (content.substr(i, stylerConst.PREFIX_LEN) == stylerConst.PREFIX_BOLD) {
+    if (content.substr(i, stylerConst.PREFIX_LEN) == PREFIX) {
       i += stylerConst.PREFIX_LEN;
       start = i;
 
       for (; i < content.length; i++) {
         if (content[i] == stylerConst.POSTMARK) {
-        if (content.substr(i, stylerConst.POSTFIX_LEN) == stylerConst.POSTFIX_BOLD) {
+        if (content.substr(i, stylerConst.POSTFIX_LEN) == POSTFIX) {
           result.push({start: start, end: i - 1});
           i = i + stylerConst.POSTFIX_LEN - 1;
           break;
