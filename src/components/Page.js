40d84244ece1dc4d6a3621b7b5d3897ee38cc147
res876ttb <someone10851222@gmail.com>
2021-05-03 23:34:50 +0800

    Add: Add copy button to copy current editor state into clipboard.

--- src/components/Page.js
+++ src/components/Page.js
@@ -13,7 +13,7 @@ import { useDispatch } from 'react-redux';
 import {
   Editor,
   EditorState,
-  // convertToRaw,
+  convertToRaw,
   convertFromRaw,
 } from 'draft-js';
 
@@ -68,7 +68,6 @@ const Page = props => {
 
   // onChange
   const updateEditor = editorState => {
-    // console.log(JSON.stringify(convertToRaw(editorState.getCurrentContent())));
     if (!readOnly) setEditorState(editorState);
   };
 
@@ -113,9 +112,72 @@ const Page = props => {
         // placeholder={'Write something here...'}
       />
       <div className='geeke-pageBottom'></div>
+      <button onClick={e => {
+        copyTextToClipboard(JSON.stringify(convertToRaw(editorState.getCurrentContent())));
+      }}>Copy content</button>
       {/* TODO: make it work: drop on bottom of a page */}
     </div>
   )
 }
 
 export default Page;
+
+// For debug only
+// Copied from https://stackoverflow.com/a/30810322/6868122
+function copyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+
+  //
+  // *** This styling is an extra step which is likely not required. ***
+  //
+  // Why is it here? To ensure:
+  // 1. the element is able to have focus and selection.
+  // 2. if the element was to flash render it has minimal visual impact.
+  // 3. less flakyness with selection and copying which **might** occur if
+  //    the textarea element is not visible.
+  //
+  // The likelihood is the element won't even render, not even a
+  // flash, so some of these are just precautions. However in
+  // Internet Explorer the element is visible whilst the popup
+  // box asking the user for permission for the web page to
+  // copy to the clipboard.
+  //
+
+  // Place in the top-left corner of screen regardless of scroll position.
+  textArea.style.position = 'fixed';
+  textArea.style.top = 0;
+  textArea.style.left = 0;
+
+  // Ensure it has a small width and height. Setting to 1px / 1em
+  // doesn't work as this gives a negative w/h on some browsers.
+  textArea.style.width = '2em';
+  textArea.style.height = '2em';
+
+  // We don't need padding, reducing the size if it does flash render.
+  textArea.style.padding = 0;
+
+  // Clean up any borders.
+  textArea.style.border = 'none';
+  textArea.style.outline = 'none';
+  textArea.style.boxShadow = 'none';
+
+  // Avoid flash of the white box if rendered for any reason.
+  textArea.style.background = 'transparent';
+
+
+  textArea.value = text;
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Copying text command was ' + msg);
+  } catch (err) {
+    console.log('Oops, unable to copy');
+  }
+
+  document.body.removeChild(textArea);
+}
