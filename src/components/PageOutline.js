217ac6ead3f776b5ff0f7d920df77dde7168d60a
res876ttb <someone10851222@gmail.com>
2021-08-25 20:50:38 +0800

    Add:
    - TOC with dynamic highlight (according to current scroll position)
    - Tooltip delay for inline link
    - Tooltip for inline math style

--- src/components/PageOutline.js
+++ src/components/PageOutline.js
@@ -6,10 +6,13 @@
 /*************************************************
  * React Components
  *************************************************/
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box } from '@material-ui/core';
 import { withStyles } from '@material-ui/styles';
 import Tooltip from '@material-ui/core/Tooltip';
+import debounce from 'lodash/debounce';
+import { useSelector } from 'react-redux';
+import { constBlockType } from '../constant';
 
 /*************************************************
  * Utils & States
@@ -22,6 +25,9 @@ import Tooltip from '@material-ui/core/Tooltip';
 /*************************************************
  * Constant
  *************************************************/
+// NOTE: adjust **topOffset** when the top padding is adjusted...
+const topOffset = 164;
+
 const PageOutlineBox = withStyles({
   root: {
     marginLeft: '0px',
@@ -37,10 +43,98 @@ const PageOutlineTooltip = withStyles({
  * Main components
  *************************************************/
 const PageOutline = (props) => {
+  // Props
   const pageUuid = props.pageUuid;
 
   // Constants
   const editorId = `geeke-editor-${pageUuid}`;
+
+  // States & Reducers
+  const editorState = useSelector((state) => state.editor.cachedPages.get(pageUuid)?.get('content'));
+  const [focusHeading, setFocusHeading] = useState(null);
+
+  // Register function to onscroll
+  const handleScrollEnd = debounce(() => {
+    // const curY = window.scrollY + topOffset;
+    const curY = topOffset;
+
+    const closestBsearchIdx = (blocks) => {
+      // Find the element that is less then target but closest to the target
+      let start = 0;
+      let end = blocks.length;
+      let middle = -1;
+      let lastStart = start;
+      let lastEnd = end;
+
+      while (start < end) {
+        middle = Math.floor((start + end) / 2);
+        let offsetTop = blocks[middle].getBoundingClientRect().top;
+
+        if (offsetTop === curY) {
+          lastStart = -1;
+          return middle;
+        } else if (offsetTop < curY) {
+          start = middle;
+        } else {
+          end = middle;
+        }
+
+        if (lastStart === start && lastEnd === end) break;
+
+        lastStart = start;
+        lastEnd = end;
+      }
+
+      if (lastStart >= 0) {
+        if (blocks[lastEnd].getBoundingClientRect().top < curY) return lastEnd;
+        else return lastStart;
+      } else {
+        return middle;
+      }
+    };
+
+    const editorDom = document.getElementById(editorId);
+    const blocks = editorDom.children[0].children[0].children[0].children[0].children;
+    if (!blocks || blocks.length === 0) {
+      console.error('Known issue! Unable to find block!');
+      return;
+    }
+
+    // Find blocks that at top
+    const targetBlockIdx = closestBsearchIdx(blocks);
+
+    // Iterate back to find the heading block and its blockId
+    if (!editorState) {
+      console.error('EditorState is undefined!!!!!');
+      return;
+    }
+    const contentState = editorState.getCurrentContent();
+    let curIdx = targetBlockIdx;
+
+    while (curIdx >= 0) {
+      const curBlockKey = blocks[curIdx].getAttribute('data-offset-key').split('-')[0];
+      if (!curBlockKey) {
+        console.error('Unable to find block key from DOM');
+        return;
+      }
+      const curBlock = contentState.getBlockForKey(curBlockKey);
+      if (curBlock.getType() === constBlockType.heading) break;
+      curIdx--;
+    }
+
+    // If curIdx >= 0, it means that this block is a heading block
+    if (curIdx < 0) return;
+    const blockId = blocks[curIdx]?.children[0]?.children[1]?.id;
+    if (!blockId) console.error('Unable to find ID of the heading block!');
+    setFocusHeading(blockId);
+  }, 500);
+  useEffect(() => {
+    // For hot reloading
+    // But if function handleScrollEnd is changed, this method does not work...
+    window.removeEventListener('scroll', handleScrollEnd);
+
+    window.addEventListener('scroll', handleScrollEnd);
+  }, []); // eslint-disable-line
 
   // Find all heading block
   const editorDom = document.getElementById(editorId);
@@ -59,9 +153,12 @@ const PageOutline = (props) => {
 
     let headingLevel = Math.min(previousHeadingLevel + 1, parseInt(headingType.slice(1)));
     let headingContent = heading.textContent;
+    let focus = headingId === focusHeading || (!focusHeading && i === 0);
 
     previousHeadingLevel = headingLevel;
-    toc.push(<PageOutlineList key={i} content={headingContent} level={headingLevel} headingId={headingId} />);
+    toc.push(
+      <PageOutlineList key={i} content={headingContent} level={headingLevel} headingId={headingId} focus={focus} />,
+    );
   }
 
   // Render the result
@@ -69,7 +166,7 @@ const PageOutline = (props) => {
     <div className="geeke-pageOutline-wrapper" style={{ left: 0 }}>
       <PageOutlineBox display="flex" alignItems="center" p={1} m={1} sx={{ height: '100%' }}>
         <Box>
-          <div style={{ position: 'relative', width: 150 }}>{toc}</div>
+          <div style={{ width: 150 }}>{toc}</div>
         </Box>
       </PageOutlineBox>
     </div>
@@ -78,7 +175,10 @@ const PageOutline = (props) => {
 
 const PageOutlineList = (props) => {
   return (
-    <div className="geeke-pageOutline-item" style={{ marginLeft: `${(props.level - 1) * 0.8}rem` }}>
+    <div
+      className="geeke-pageOutline-item"
+      style={{ marginLeft: `${(props.level - 1) * 0.8}rem`, color: props.focus ? 'blue' : 'unset' }}
+    >
       <PageOutlineTooltip title={props.content} enterDelay={500} placement="top-start">
         <a href={`#${props.headingId}`}>{props.content}</a>
       </PageOutlineTooltip>
--- src/tests/Misc.test.js
+++ src/tests/Misc.test.js
@@ -42,3 +42,60 @@ describe(checkOverlap, () => {
     expect(checkOverlap(3, 5, 2, 6, true)).toBeTruthy();
   });
 });
+
+describe('closestBsearchIdx', () => {
+  const closestBsearchIdx = (blocks, target) => {
+    // Find the element that is less then target but closest to the target
+    let start = 0;
+    let end = blocks.length;
+    let middle = -1;
+    let lastStart = start;
+    let lastEnd = end;
+
+    while (start < end) {
+      middle = Math.floor((start + end) / 2);
+
+      if (blocks[middle] === target) {
+        lastStart = -1;
+        return middle;
+      } else if (blocks[middle] < target) {
+        start = middle;
+      } else {
+        end = middle;
+      }
+
+      if (lastStart === start && lastEnd === end) break;
+
+      lastStart = start;
+      lastEnd = end;
+    }
+
+    if (lastStart >= 0) {
+      if (blocks[lastEnd] < target) return lastEnd;
+      else return lastStart;
+    } else {
+      return middle;
+    }
+  };
+
+  test('Test', () => {
+    expect(closestBsearchIdx([0, 1, 2, 3], 0)).toBe(0);
+    expect(closestBsearchIdx([-1, 0, 1, 2, 3], 0)).toBe(1);
+    expect(closestBsearchIdx([-2, -1, 0, 1, 2], 0)).toBe(2);
+    expect(closestBsearchIdx([-3, -2, -1, 0], 0)).toBe(3);
+    expect(closestBsearchIdx([-5, -4, -3, -2, -1], 0)).toBe(4);
+    expect(closestBsearchIdx([-4, -3, -2, -1], 0)).toBe(3);
+    expect(closestBsearchIdx([-3, -2, -1], 0)).toBe(2);
+    expect(closestBsearchIdx([-2, -1], 0)).toBe(1);
+    expect(closestBsearchIdx([-1], 0)).toBe(0);
+    expect(closestBsearchIdx([0], 0)).toBe(0);
+    expect(closestBsearchIdx([0, 1], 0)).toBe(0);
+    expect(closestBsearchIdx([1], 0)).toBe(0);
+    expect(closestBsearchIdx([1, 2], 0)).toBe(0);
+    expect(closestBsearchIdx([1, 2, 3], 0)).toBe(0);
+    expect(closestBsearchIdx([1, 2, 3, 4], 0)).toBe(0);
+    expect(closestBsearchIdx([1, 2, 3, 4, 5], 0)).toBe(0);
+    expect(closestBsearchIdx([-4, -2, 1, 3, 5], 0)).toBe(1);
+    expect(closestBsearchIdx([-4, -2, 0, 3, 5], 0)).toBe(2);
+  });
+});
